f1e4a52fc0c36180db89324a31607e1de483df0d
Solitude <75237025+xczcdjx@users.noreply.github.com>
2026-01-05 08:39:01 +0000

    task

--- dynamicForm-Vue2-old/src/router/index.js
+++ dynamicForm-Vue2-old/src/router/index.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 import routes from "./routes";
-Vue.use(VueRouter)
-const router=new VueRouter({
-  mode:'history',
-  routes
-})
-export default router
+Vue.use(VueRouter);
+const router = new VueRouter({
+  mode: "history",
+  routes,
+});
+export default router;
