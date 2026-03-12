d876e4cd0a03cfbb2665d490782cb20ffd4435e5
Solitude <xczcdjx@126.com>
2026-03-12 11:49:28 +0800

    page2

--- dynamicForm-Vue2-old/config/index.js
+++ dynamicForm-Vue2-old/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/packageTest/dynamicForm-Vue2-old/',
 
     /**
      * Source Maps
